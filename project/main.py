bca58cb28e5abd75ce96835eab7f7903a8a65626
ok <przywarty.it@gmail.com>
2018-05-24 13:05:22 +0200

    Add GUI.

--- project/main.py
+++ project/main.py
@@ -79,24 +79,36 @@ def video_stream_detection():
     process_this_frame = True
     while True:
         ret, frame = video_capture.read()
-        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        # small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        # rgb_small_frame = small_frame[:, :, ::-1]
         if process_this_frame:
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            # img_rgb_eq = equalize_histogram(frame)
+            face_locations = face_recognition.face_locations(frame)
+            face_encodings = face_recognition.face_encodings(frame, face_locations)
             face_names = []
             for face_encoding in face_encodings:
-                prediction = clf.predict(face_encoding.reshape(1, -1))[0]
-                face_names.append(prediction)
+                face_encoding_reshaped = face_encoding.reshape(1, -1)
+                probability = clf.predict_proba(face_encoding_reshaped)[0]
+                if any(p > 0.8 for p in probability):
+                    prob_string = ""
+                    for p in probability:
+                        if p > 0.8:
+                            prob_string = str(p)
+                    prediction = clf.predict(face_encoding_reshaped)[0]
+                    person_text = "{} {}".format(prediction, prob_string)
+                else:
+                    person_text = "Unknown"
+                face_names.append(person_text)
         process_this_frame = not process_this_frame
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            top *= 2
-            right *= 2
-            bottom *= 2
-            left *= 2
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # top *= 2
+            # right *= 2
+            # bottom *= 2
+            # left *= 2
+            cv2.rectangle(frame, (left, top), (right, bottom), (152, 0, 152), 3)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(img=frame, text=name, org=(left + 6, bottom - 6), fontFace=font, 
+                        color=(255, 255, 255), bottomLeftOrigin=False, fontScale=0.5)
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -104,7 +116,34 @@ def video_stream_detection():
     cv2.destroyAllWindows()
 
 
-video_stream_detection()
+from tkinter import Tk, Button, Label, PhotoImage, Canvas
+
+class Window:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+        master.geometry("810x434")
+
+        background_image = PhotoImage(file="bg.png")
+        background_label = Label(self.master, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.label = Label(master, text="Facial Recognition", fg="#00ff00", bg="#333248", font=("Helvetica", 16))
+        self.label.grid(row=0, sticky='s')
+        self.label = Label(master, text="Adam Przywarty, Maciej Korzeniewski", fg="#00ff00", bg="#333248", font=("Helvetica", 12))
+        self.label.grid(row=1, sticky='s')
+        self.greet_button = Button(master, text="RUN WEBCAM!", command=self.greet, fg="#333248", bg="#00ff00", font=("Helvetica", 12))
+        self.greet_button.grid(row=2, sticky='s')
+
+        self.master.mainloop()
+
+    def greet(self):
+        video_stream_detection()
+
+root = Tk()
+Window(root)
+
+# video_stream_detection()
 # load_classifier()
 # pickle_x, pickle_y = load_encodings_from_pickle()
 # create_classifier(pickle_x, pickle_y)
