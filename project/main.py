7a926ec966568aaf5bfefaf8053eaad77a0a5c99
Adam Przywarty <przywarty.it@gmail.com>
2018-05-30 15:18:01 +0200

    Add documentation.

--- project/main.py
+++ project/main.py
@@ -11,6 +11,20 @@ from tqdm import tqdm
 
 
 def create_encodings_pickle():
+    # 1. For each image in the dataset, generate the face encoding.
+    #    It is done by using the face_recognition Dlib API, which 
+    #    with the function called "load_image_file()" performs
+    #    detection of faces on the image and central face alignment.
+    # 2. Using the face_encodings() again from Dlib API, pass it
+    #    the previously aligned face and it will run it through 
+    #    a previously trained neural network and generate 128 encodings
+    #    for that face in the photo.
+    # 3. Lastly, create a list of photos of each class and store in dict.
+    #    Now, the dictionary is saved to a pickle file.
+    #    data = {
+    #      'maciek': [128 embeddings, 128 embeddings, 128 embeddings, ...],
+    #      'adam': [128 embeddings, 128 embeddings, 128 embeddings, ...],
+    #    }
     directory = os.fsencode("data/raw")
     data = {}
     for file in os.listdir(directory):
@@ -27,6 +41,10 @@ def create_encodings_pickle():
 
 
 def load_encodings_from_pickle():
+    # Loading of the pickle saved in create_encodings_pickle() function.
+    # The result is:
+    #     X -> a numpy array of encodings of all classes
+    #     y -> a numpy array of labels of each encoding
     with open("encodings.pickle", "rb") as f:
         data = pickle.load(f)
     X, y = [], []
@@ -38,8 +56,9 @@ def load_encodings_from_pickle():
 
 
 def create_classifier(X, y):
+    # Create classifier by training the support vector machine on the previously
+    # prepared dataset. Save the classifier to a file.
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    # LinearSVC_classifier = SklearnClassifier(SVC(kernel='linear',probability=True))
     clf = SVC(kernel='linear', probability=True, random_state=42)
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
@@ -79,32 +98,33 @@ def video_stream_detection():
     process_this_frame = True
     while True:
         ret, frame = video_capture.read()
-        # small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        # rgb_small_frame = small_frame[:, :, ::-1]
+        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        rgb_small_frame = small_frame[:, :, ::-1]
         if process_this_frame:
-            # img_rgb_eq = equalize_histogram(frame)
-            face_locations = face_recognition.face_locations(frame)
-            face_encodings = face_recognition.face_encodings(frame, face_locations)
+            img_rgb_eq = equalize_histogram(rgb_small_frame)
+            face_locations = face_recognition.face_locations(img_rgb_eq)
+            face_encodings = face_recognition.face_encodings(img_rgb_eq, face_locations)
             face_names = []
             for face_encoding in face_encodings:
                 face_encoding_reshaped = face_encoding.reshape(1, -1)
                 probability = clf.predict_proba(face_encoding_reshaped)[0]
-                if any(p > 0.8 for p in probability):
+                if any(p > 0.85 for p in probability):
                     prob_string = ""
                     for p in probability:
-                        if p > 0.8:
-                            prob_string = str(p)
+                        if p > 0.85:
+                            p *= 100
+                            prob_string = str(p)[:4]
                     prediction = clf.predict(face_encoding_reshaped)[0]
-                    person_text = "{} {}".format(prediction, prob_string)
+                    person_text = "{} {}%".format(prediction, prob_string)
                 else:
                     person_text = "Unknown"
                 face_names.append(person_text)
         process_this_frame = not process_this_frame
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # top *= 2
-            # right *= 2
-            # bottom *= 2
-            # left *= 2
+            top *= 2
+            right *= 2
+            bottom *= 2
+            left *= 2
             cv2.rectangle(frame, (left, top), (right, bottom), (152, 0, 152), 3)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(img=frame, text=name, org=(left + 6, bottom - 6), fontFace=font, 
@@ -142,8 +162,3 @@ class Window:
 
 root = Tk()
 Window(root)
-
-# video_stream_detection()
-# load_classifier()
-# pickle_x, pickle_y = load_encodings_from_pickle()
-# create_classifier(pickle_x, pickle_y)
