64e88ba636e78d5779cf3f0201866270bc1f8edd
Adam Przywarty <przywarty.it@gmail.com>
2018-06-13 14:49:15 +0200

    Refactor detection and recognition.

--- project/main.py
+++ project/main.py
@@ -3,23 +3,47 @@ import pickle
 import time
 
 import cv2
-import face_recognition
+import dlib
+import face_recognition_models
 import numpy as np
+import PIL.Image
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from tqdm import tqdm
+from tkinter import Tk, Button, Label, PhotoImage, Canvas
+
+
+face_detector = dlib.get_frontal_face_detector()
+
+
+def load_image_file(file):
+    # Loads an image file (.jpg, .png, etc) into a numpy array
+    im = PIL.Image.open(file)
+    im = im.convert('RGB')
+    return np.array(im)
+
+
+def _raw_face_landmarks(face_image):
+    # Using Dlib face detection, pose estimation and recognition models
+    # * find face locations (its a list of all faces in the image)
+    # * align centrally the detected faces
+    face_locations = face_detector(face_image, 1)
+    pose_predictor_5_point = dlib.shape_predictor("shape_predictor.dat")
+    return [pose_predictor_5_point(face_image, face_location) for face_location in face_locations]
+
+
+def face_encodings(face_image):
+    # Run the detected faces through a pre-trained neural network and get 128 face encodings for each
+    raw_landmarks = _raw_face_landmarks(face_image)
+    face_encoder = dlib.face_recognition_model_v1("face_encoder.dat")
+    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, 1)) for raw_landmark_set in raw_landmarks]
 
 
 def create_encodings_pickle():
-    # 1. For each image in the dataset, generate the face encoding.
-    #    It is done by using the face_recognition Dlib API, which 
-    #    with the function called "load_image_file()" performs
-    #    detection of faces on the image and central face alignment.
-    # 2. Using the face_encodings() again from Dlib API, pass it
-    #    the previously aligned face and it will run it through 
+    # 1. Using the face_encodings(), it will run it through 
     #    a previously trained neural network and generate 128 encodings
     #    for that face in the photo.
-    # 3. Lastly, create a list of photos of each class and store in dict.
+    # 2 Lastly, create a list of photos of each class and store in dict.
     #    Now, the dictionary is saved to a pickle file.
     #    data = {
     #      'maciek': [128 embeddings, 128 embeddings, 128 embeddings, ...],
@@ -33,10 +57,10 @@ def create_encodings_pickle():
         data[filename] = []
         print("Processing faces of {}.".format(filename))
         for image in tqdm(os.listdir(person_directory)):
-            loaded_image = face_recognition.load_image_file(os.path.join(person_directory, image))
-            encodings = face_recognition.face_encodings(loaded_image)
+            loaded_image = load_image_file(os.path.join(person_directory, image))
+            encodings = face_encodings(loaded_image)
             encodings and data[filename].append(encodings[0])
-    with open("encodings.pickle", "wb") as f:
+    with open("encodings1.pickle", "wb") as f:
         pickle.dump(data, f)
 
 
@@ -45,7 +69,7 @@ def load_encodings_from_pickle():
     # The result is:
     #     X -> a numpy array of encodings of all classes
     #     y -> a numpy array of labels of each encoding
-    with open("encodings.pickle", "rb") as f:
+    with open("encodings1.pickle", "rb") as f:
         data = pickle.load(f)
     X, y = [], []
     for key in data:
@@ -63,14 +87,14 @@ def create_classifier(X, y):
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print("Finished training, score {}".format(score))
-    with open("classifier.h5", "wb") as f:
+    with open("classifier1.h5", "wb") as f:
         pickle.dump(clf, f)
 
 
 def load_classifier():
     with open("classifier.h5", "rb") as f:
         clf = pickle.load(f)
-    loaded_image = face_recognition.load_image_file("data/testing-adam.png")
+    loaded_image = load_image_file("data/testing-adam.png")
     encodings = face_recognition.face_encodings(loaded_image)
     prediction = None
     if encodings:
@@ -86,12 +110,28 @@ def equalize_histogram(image):
     return cv2.cvtColor(img_y_cr_cb_eq, cv2.COLOR_YCR_CB2BGR)
 
 
+def _rect_to_css(rect):
+    # Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
+    return rect.top(), rect.right(), rect.bottom(), rect.left()
+
+
+def _trim_css_to_bounds(css, image_shape):
+    # Make sure a tuple in (top, right, bottom, left) order is within the bounds of the image.
+    return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
+
+
+def get_face_locations(img):
+    # Get a box tuple for each face in the image
+    return [_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in face_detector(img, 1)]
+
+
 def process_the_frame(rgb_small_frame, clf):
-    img_rgb_eq = equalize_histogram(rgb_small_frame)
-    face_locations = face_recognition.face_locations(img_rgb_eq)
-    face_encodings = face_recognition.face_encodings(img_rgb_eq, face_locations)
+    # img_rgb_eq = equalize_histogram(rgb_small_frame)
+    img_rgb_eq = rgb_small_frame
+    face_locations = get_face_locations(img_rgb_eq)
+    encodings = face_encodings(img_rgb_eq)
     face_names = []
-    for face_encoding in face_encodings:
+    for face_encoding in encodings:
         face_encoding_reshaped = face_encoding.reshape(1, -1)
         probability = clf.predict_proba(face_encoding_reshaped)[0]
         if any(p > 0.85 for p in probability):
@@ -117,12 +157,12 @@ def display_face_box(frame, top, right, bottom, left, name):
     cv2.rectangle(frame, (left, top), (right, bottom), (152, 0, 152), 3)
     font = cv2.FONT_HERSHEY_DUPLEX
     cv2.putText(img=frame, text=name, org=(left + 6, bottom - 6), fontFace=font, 
-                color=(255, 255, 255), bottomLeftOrigin=False, fontScale=0.5)
+                color=(152, 0, 152), bottomLeftOrigin=False, fontScale=0.5)
     return frame
 
 
 def video_stream_detection():
-    with open("classifier.h5", "rb") as f:
+    with open("classifier1.h5", "rb") as f:
         clf = pickle.load(f)
     video_capture = cv2.VideoCapture(0)
     video_capture.set(3, 400)
@@ -148,8 +188,6 @@ def video_stream_detection():
     cv2.destroyAllWindows()
 
 
-from tkinter import Tk, Button, Label, PhotoImage, Canvas
-
 class Window:
     def __init__(self, master):
         self.master = master
@@ -174,3 +212,11 @@ class Window:
 
 root = Tk()
 Window(root)
+
+
+def save_classifier_to_file():
+    create_encodings_pickle()
+    a, b = load_encodings_from_pickle()
+    create_classifier(a, b)
+
+# save_classifier_to_file()
